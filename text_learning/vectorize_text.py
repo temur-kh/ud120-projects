88a093ceff5d74a1e400b1c91c75f56209b046e7
Temur <tim_1998@bk.ru>
2018-12-13 14:16:44 +0300

    Implement getting lists of stemmed emails, remove temp_counter limit, fit TfIdfVectorizer with word_data

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,35 +41,29 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
             email = open(path, "r")
+            text = parseOutText(email)
 
-            ### use parseOutText to extract the text from the opened email
+            for word in ["sara", "shackleton", "chris", "germani"]:
+                text = text.replace(word, "")
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
-
-            ### append the text to word_data
-
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            word_data.append(text)
+            from_data.append(0 if name == "sara" else 1)
 
             email.close()
 
 print "emails processed"
 from_sara.close()
 from_chris.close()
+# print word_data[152]  # testing
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
-
-
-
-### in Part 4, do TfIdf vectorization here
-
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english")
+vectorizer.fit(word_data)
+print len(vectorizer.get_feature_names())
